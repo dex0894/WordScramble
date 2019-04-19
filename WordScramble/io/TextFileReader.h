40ac3f22dd43028fdfb33e17b5cbe7411491a8e0
Dexter <dtarver1@my.westga.edu>
2019-04-19 01:47:19 -0400

    Added dictionary.txt as a resource. Implemented new functionality in WordScrambleController. Implemented the TextFileReaderClass.

---
+++ WordScramble/io/TextFileReader.h
@@ -0,0 +1,29 @@
+#ifndef TEXTFILEREADER_H
+#define TEXTFILEREADER_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+namespace io
+{
+
+
+class TextFileReader
+{
+    private:
+        vector<string> wordCollection;
+    public:
+        TextFileReader();
+        virtual ~TextFileReader();
+
+        vector<string> readFile();
+
+
+
+};
+}
+#endif // TEXTFILEREADER_H
