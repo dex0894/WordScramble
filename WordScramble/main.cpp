a4db40ba7decc5f557ccfea51f7e9bcd3baa9b81
Dexter <dtarver1@my.westga.edu>
2019-04-20 10:49:10 -0400

    Fixed LetterGenerator.

--- WordScramble/main.cpp
+++ WordScramble/main.cpp
@@ -6,6 +6,7 @@ using namespace view;
 
 int main (int argc, char ** argv)
 {
+    srand(time(NULL));
     WordScrambleWindow mainWindow(540, 500, "Word Scramble by Kevin Flynn and Dexter Tarver");
     mainWindow.show();
 
